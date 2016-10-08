38b39c53f670bd90f81f987ebe5a872b8bc5cca0
Alex Gofman <gofmanalex13@gmail.com>
2016-10-08 15:13:41 +0400

    Sign in custom classes complete

---
+++ SocialNetworkDevSlo/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  SocialNetworkDevSlo
+//
+//  Created by Alex Hoffman on 10/8/16.
+//  Copyright © 2016 Alex Hoffman. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
