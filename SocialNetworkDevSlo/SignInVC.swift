3514bca6393b5689489ae35af8ebe9e6aaac3033
Alex Gofman <gofmanalex13@gmail.com>
2016-10-08 14:34:52 +0400

    Sign in UI layout complete

--- SocialNetworkDevSlo/SignInVC.swift
+++ SocialNetworkDevSlo/SignInVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SignInVC.swift
 //  SocialNetworkDevSlo
 //
 //  Created by Alex Hoffman on 10/8/16.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SignInVC: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
