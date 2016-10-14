a5574a6fb04f855ebe5fd92b774ccca511c73f77
Alex Gofman <gofmanalex13@gmail.com>
2016-10-14 13:26:51 +0400

    add likes functionality

--- SocialNetworkDevSlo/FeedVC.swift
+++ SocialNetworkDevSlo/FeedVC.swift
@@ -72,11 +72,10 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIIm
 			//var img: UIImage!
 			if let img = FeedVC.imageCache.object(forKey: post.imageUrl as NSString) {
 				cell.configureCell(post: post, img: img)
-				return cell
 			} else {
 				cell.configureCell(post: post)
-				return cell
 			}
+			return cell
 			
 		} else {
 			return PostCell()
